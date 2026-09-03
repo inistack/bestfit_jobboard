2cfdf460ccb036e10c8dc18afc9d1be72ff8f6a8
Iniubong Arthur <arthurshur@gmail.com>
2026-09-03 21:29:26 +0000

    JWT extension

--- jobboard/__init__.py
+++ jobboard/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_smorest import Api
-from .extensions import db, migrate
+from .extensions import db, migrate, jwt
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
     db.init_app(app)
+    jwt.init_app(app)
     migrate.init_app(app, db)
 
     with app.app_context():
